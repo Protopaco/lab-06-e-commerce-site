b943577df00c2b667c89add7d757c09c36b895f4
Paul Stevens <twinkisan@hotmail.com>
2020-10-05 14:55:15 -0700

    created 'canineFashion' data structure

---
+++ products/canine-fashion.js
@@ -0,0 +1,51 @@
+const CF201171F067034 = {
+    id: 'CF201171F067034',
+    name: 'Black Poldo Dog Couture Edition Insulated Jacket',
+    description: 'Sleeveless insulated quilted nylon jacket in black. Stand collar. Press-stud closure at front. Tonal rubberized logo patch, logo plaque, and leash opening at back. Fully lined. Silver-tone hardware.',
+    image_src: 'puffy-black.jpg',
+    image_alt: 'black puffy dog jacket',
+    category: 'canine-fashion',
+    price: '455'
+}
+
+const CF201171F067035 = {
+    id: 'CF201171F067035',
+    name: 'Multicolor Poldo Dog Couture Edition Striped Jacket',
+    description: 'Sleeveless insulated quilted nylon jacket striped in red, white, and navy. Detachable hood featuring press-stud fastening and bungee-style drawstring at stand collar. Press-stud closure at front. Logo patch and logo plaque. Fully lined. Silver-tone hardware.',
+    image_src: 'striped-jacket.jpg',
+    image_alt: 'striped puffy dog jacket',
+    category: 'canine-fashion',
+    price: '575'
+}
+
+const CF201171F591042 = {
+    id: 'CF201171F591042',
+    name: 'Reversible Red Poldo Dog Couture Edition Sweater Knit Jacket',
+    description: 'Reversible sleeveless insulated quilted nylon jacket featuring graphic pattern in black, white, and red. Detachable hood with bungee-style drawstring and press-stud fastening at stand collar. Press-stud closure at front. Felted logo patch and logo plaque at back. Silver-tone hardware',
+    image_src: 'red-poldo.jpg',
+    image_alt: 'reversible dog jacket',
+    category: 'canine-fashion',
+    price: '575'
+}
+
+const CF201171F067037 = {
+    id: 'CF201171F067037',
+    name: 'Blue Poldo Dog Couture Edition Waterproof Coat',
+    description: 'Long sleeve translucent polyurethane coat in blue. Detachable hood with rib knit trim at rib knit stand collar. Press-stud closure at front. Bungee-style drawstring at hem. Rubberized logo patch and logo plaque at back. Unlined. Silver-tone hardware',
+    image_src: 'blue-waterproof-coast.jpg',
+    image_alt: 'waterproof dog jacket',
+    category: 'canine-fashion',
+    price: '455'
+}
+
+const CF201171F067033 = {
+    id: 'CF201171F067033',
+    name: 'Blue Poldo Dog Couture Edition Insulated Jacket',
+    description: 'Sleeveless insulated quilted nylon jacket in blue. Stand collar. Press-stud closure at front. Tonal rubberized logo patch, logo plaque, and leash opening at back. Fully lined. Silver-tone hardware.',
+    image_src: 'puffy-blue.jpg',
+    image_alt: 'blue puffy dog jacket',
+    category: 'canine-fashion',
+    price: '455'
+}
+
+export const canineFashion = [CF201171F067034, CF201171F067035, CF201171F591042, CF201171F067037, CF201171F067033];
