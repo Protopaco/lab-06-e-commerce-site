d6cb3ef177bb7610e8df36ee8f999b7f67624919
Paul Stevens <twinkisan@hotmail.com>
2020-10-06 14:15:55 -0700

    Hardcoded cart array under /data/cart.js

---
+++ data/cart.js
@@ -0,0 +1,16 @@
+
+export const cart = [
+    {
+    id: 'CF201171F067034',
+    quantity: 2,
+},
+{
+    id: 'CF201171F591042',
+    quantity: 1,
+},
+{
+    id:'CF201927F095014',
+    quantity: 5,
+}
+
+]
