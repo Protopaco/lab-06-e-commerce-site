4b249267ed339ad0ea24846c378e2d0290162aa0
Paul Stevens <twinkisan@hotmail.com>
2020-10-05 17:13:06 -0700

    tweaked styling and added more products

--- products/canine-fashion.js
+++ products/canine-fashion.js
@@ -1,6 +1,6 @@
-export const CF201171F067034 = {
+const CF201171F067034 = {
     id: 'CF201171F067034',
-    name: 'Black Poldo Dog Couture Edition Insulated Jacket',
+    name: 'Black Couture Edition Insulated Jacket',
     description: 'Sleeveless insulated quilted nylon jacket in black. Stand collar. Press-stud closure at front. Tonal rubberized logo patch, logo plaque, and leash opening at back. Fully lined. Silver-tone hardware.',
     image_src: 'puffy-black.jpg',
     image_alt: 'black puffy dog jacket',
@@ -48,4 +48,56 @@ const CF201171F067033 = {
     price: '455'
 }
 
-export const canineFashions = [CF201171F067034, CF201171F067035, CF201171F591042, CF201171F067037, CF201171F067033];
+const CF201937F067157 ={
+    id: '201937F067157',
+    name: 'Yellow Goth Dog Hoodie',
+    description: 'Long sleeve French terry hoodie in yellow. Leash opening at back collar. Logo printed in black at back.',
+    image_src: 'yellow-goth-dog-hoodie.jpg',
+    image_alt: 'neon yellow goth hoodie',
+    category: 'canine fashion',
+    price: '170',
+}
+
+const CF201967F067184 ={
+    id: '201967F067184',
+    name: 'Orange VIP Edition Dog Raincoat',
+    description: 'Long sleeve transparent PVC raincoat in orange. Press-stud closure at front. Elasticized trim featuring jacquard logo pattern in white at cuffs. Leash opening at back collar. Textile logo patch in black at back. Black hardware.',
+    image_src: 'orange-vip-raincoat.jpg',
+    image_alt: 'orange dog raincoat',
+    category: 'canine fashion',
+    price: '170',
+}
+
+const CF201937F067160 ={
+    id: '201937F067160',
+    name: 'Black Active Dog T-Shirt',
+    description: 'Sleeveless stretch nylon jersey t-shirt in black. Press-stud closure at front. Logo graphic printed in white at back. Tonal logo-embossed hardware.',
+    image_src: 'black-active-tshirt.jpg',
+    image_alt: 'black active dog t-shirt',
+    category: 'canine fashion',
+    price: '100',
+}
+
+const CF201228F067064 ={
+    id: '201228F067064',
+    name: 'Purple Nonna Turtleneck Dog Collar',
+    description: 'Rib knit dog collar in purple. Approx. 8.5" neck x 7.25" length.',
+    image_src: 'purple-nonna-turtleneck.jpg',
+    image_alt: 'purple dog turtleneck',
+    category: 'canine fashion',
+    price: '70',
+}
+
+const CF201927F095014 ={
+    id: '201927F095014',
+    name: 'SSENSE Exclusive Grey & Orange Dog Sweater',
+    description: 'Sleeveless knit wool sweater in orange featuring check pattern in grey. Rib knit crewneck collar and hem.',
+    image_src: 'argyle-sweater.jpg',
+    image_alt: 'small dog in an argyle sweater',
+    category: 'canine fashion',
+    price: '140',
+}
+
+
+
+export const canineFashions = [CF201171F067034, CF201171F067035, CF201171F591042, CF201171F067037, CF201171F067033, CF201937F067157, CF201967F067184, CF201937F067160, CF201228F067064, CF201927F095014];
